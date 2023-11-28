7cc74352e826040dd31c2bfbbc55502f57b1bdda
Mateusz Natan Molenda <91672079+Piasttorun@users.noreply.github.com>
2023-11-28 16:34:00 +0000

    changing service name

---
+++ src/main/java/com/sparta/jarjarbinks/worldproject/model/services/WorldService.java
@@ -0,0 +1,22 @@
+package com.sparta.jarjarbinks.worldproject.model.services;
+
+import com.sparta.jarjarbinks.worldproject.model.repositories.CityRepository;
+import com.sparta.jarjarbinks.worldproject.model.repositories.CountryRepository;
+import com.sparta.jarjarbinks.worldproject.model.repositories.CountrylanguageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WorldService {
+
+    private CityRepository cityRepository;
+    private CountryRepository countryRepository;
+    private CountrylanguageRepository countrylanguageRepository;
+
+    @Autowired
+    public WorldService(CityRepository cityRepository, CountryRepository countryRepository, CountrylanguageRepository countrylanguageRepository) {
+        this.cityRepository = cityRepository;
+        this.countryRepository = countryRepository;
+        this.countrylanguageRepository = countrylanguageRepository;
+    }
+}

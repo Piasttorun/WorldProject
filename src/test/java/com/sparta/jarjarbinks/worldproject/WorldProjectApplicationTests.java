00e4ad58e3d412dc895796200587d53e66994b87
Mateusz Natan Molenda <91672079+Piasttorun@users.noreply.github.com>
2023-11-28 15:49:49 +0000

    Uploading initial files

---
+++ src/test/java/com/sparta/jarjarbinks/worldproject/WorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.jarjarbinks.worldproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

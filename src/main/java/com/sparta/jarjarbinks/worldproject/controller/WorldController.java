faefd8b5440bf4e4a996b6429e861c97f09416c3
Mateusz Natan Molenda <91672079+Piasttorun@users.noreply.github.com>
2023-11-29 15:36:03 +0000

    adding controller changed

--- src/main/java/com/sparta/jarjarbinks/worldproject/controller/WorldController.java
+++ src/main/java/com/sparta/jarjarbinks/worldproject/controller/WorldController.java
@@ -1,4 +1,40 @@
 package com.sparta.jarjarbinks.worldproject.controller;
 
+import com.sparta.jarjarbinks.worldproject.model.entities.CityDTO;
+import com.sparta.jarjarbinks.worldproject.model.services.WorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class WorldController {
+
+    private final WorldService worldService;
+
+    @Autowired
+    public WorldController(WorldService worldService) {
+        this.worldService = worldService;
+    }
+
+    @DeleteMapping("/city/{id}")
+    public void deleteCity(@PathVariable Integer id) {
+    }
+
+    @PostMapping("/city")
+    public void createCity(@RequestBody CityDTO newCity) {
+    }
+
+    @GetMapping("/city")
+    public Optional<CityDTO> getCity() {
+        return null;
+    }
+
+    @PatchMapping("/city/{id}")
+    public Optional<CityDTO> patchCity(@RequestBody CityDTO newCity, @PathVariable Integer id) {
+        return null;
+    }
 }

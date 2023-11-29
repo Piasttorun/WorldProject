5002b585efbfdf15b11ac57fb62260ad81a4e000
Mateusz Natan Molenda <91672079+Piasttorun@users.noreply.github.com>
2023-11-29 16:59:26 +0000

    did Mati method

--- src/main/java/com/sparta/jarjarbinks/worldproject/model/repositories/CountryRepository.java
+++ src/main/java/com/sparta/jarjarbinks/worldproject/model/repositories/CountryRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CountryRepository extends JpaRepository<CountryDTO, String> {
+
+    CountryDTO findByCode(String code);
 }
--- src/main/java/com/sparta/jarjarbinks/worldproject/model/services/WorldService.java
+++ src/main/java/com/sparta/jarjarbinks/worldproject/model/services/WorldService.java
@@ -1,10 +1,16 @@
 package com.sparta.jarjarbinks.worldproject.model.services;
 
+import com.sparta.jarjarbinks.worldproject.model.entities.CityDTO;
+import com.sparta.jarjarbinks.worldproject.model.entities.CountryDTO;
 import com.sparta.jarjarbinks.worldproject.model.repositories.CityRepository;
 import com.sparta.jarjarbinks.worldproject.model.repositories.CountryRepository;
 import com.sparta.jarjarbinks.worldproject.model.repositories.CountrylanguageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraReactiveRepositoriesAutoConfiguration;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class WorldService {
@@ -27,6 +33,31 @@ public class WorldService {
 
 
     //Which country has the most cities? How many cites does it have? Mateusz
+
+    public CountryDTO getCountryMostCities() {
+        int freq = 0;
+        String res = "";
+
+        ArrayList<String> temp = new ArrayList<>();
+        for (CityDTO city : cityRepository.findAll()) {
+            temp.add(String.valueOf(city.getCountryCode()));
+        }
+        for (int i = 0; i < temp.size(); i++) {
+            int count = 0;
+            for (int j = i + 1; j < temp.size(); j++) {
+                if (temp.get(j) == temp.get(i)) {
+                    count++;
+                }
+            }
+            // updating our max freq of occurred string in the
+            // array of strings
+            if (count >= freq) {
+                res = temp.get(i);
+                freq = count;
+            }
+        }
+        return countryRepository.findByCode(res);
+    }
 
 
     //which 5 districts have the smallest population? Bianca

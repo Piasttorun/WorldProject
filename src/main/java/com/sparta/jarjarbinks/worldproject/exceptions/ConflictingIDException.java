b41b2e14d73e2316c8f9f54b26afbe89c77e878a
Mateusz Natan Molenda <91672079+Piasttorun@users.noreply.github.com>
2023-11-30 15:33:10 +0000

    changing exception name

--- src/main/java/com/sparta/jarjarbinks/worldproject/exceptions/ConflictingIDException.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.jarjarbinks.worldproject.exceptions;
-
-public class ConflictingIDException extends Exception {
-
-    public ConflictingIDException(String idname) {
-        super("Conflicting ID for: " + idname);
-    }
-
-}

6659e468d553aab707a2fdc68d63ad474ce62c11
Affiq <affiqrbk@gmail.com>
2023-11-30 09:09:59 +0000

    Fixed some classes and added a small test controller

--- src/main/java/com/sparta/jarjarbinks/worldproject/model/repositories/CountryRepository.java
+++ src/main/java/com/sparta/jarjarbinks/worldproject/model/repositories/CountryRepository.java
@@ -4,7 +4,11 @@ import com.sparta.jarjarbinks.worldproject.model.entities.CountryDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CountryRepository extends JpaRepository<CountryDTO, String> {
+
+    public List<CountryDTO> findByCode(String countryCode);
 
 }
--- src/main/java/com/sparta/jarjarbinks/worldproject/model/repositories/CountrylanguageRepository.java
+++ src/main/java/com/sparta/jarjarbinks/worldproject/model/repositories/CountrylanguageRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 public interface CountrylanguageRepository extends JpaRepository<CountrylanguageDTO, CountrylanguageIdDTO> {
 
-    void deleteById(Integer id);
-    List<CountrylanguageDTO> findAllByCountryCodeAndOrderByPercentageDesc(CountryDTO countryDTO);
+    void deleteById(CountrylanguageIdDTO countrylanguageIdDTO);
+    List<CountrylanguageDTO> findAllByCountryCodeOrderByPercentageDesc(CountryDTO countryDTO);
 }

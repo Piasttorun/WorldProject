0b50f9cd0d0141bb98071fe3ec0663abba767d1f
Uyi Aguebor <uyiaguebor@gmail.com>
2023-11-30 16:21:08 +0000

    tried adding delete testing and adding methods to WorldController.

--- src/main/java/com/sparta/jarjarbinks/worldproject/controller/TestController.java
+++ src/main/java/com/sparta/jarjarbinks/worldproject/controller/TestController.java
@@ -1,37 +1,40 @@
-package com.sparta.jarjarbinks.worldproject.controller;
-
-import com.sparta.jarjarbinks.worldproject.model.entities.CountryDTO;
-import com.sparta.jarjarbinks.worldproject.model.repositories.CountryRepository;
-import com.sparta.jarjarbinks.worldproject.model.services.WorldService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
-
-@RestController
-public class TestController {
-
-    private final WorldService worldService;
-    private final CountryRepository countryRepository;
-
-    @Autowired
-    public TestController(WorldService worldService, CountryRepository countryRepository) {
-        this.worldService = worldService;
-        this.countryRepository = countryRepository;
-    }
-
-    @GetMapping("/test/{countrycode}")
-    public int TestEndPoint(@PathVariable String countrycode) {
-        Optional<CountryDTO> country = worldService.getCountryByCode(countrycode);
-        if (country.isEmpty()) {
-            // Throw exception
-            return 0;
-        } else {
-            return worldService.getNumberOfPopularLanguageSpeakers(country.get());
-        }
-    }
-
-}
+//package com.sparta.jarjarbinks.worldproject.controller;
+//
+//import com.sparta.jarjarbinks.worldproject.model.entities.CountryDTO;
+//import com.sparta.jarjarbinks.worldproject.model.repositories.CountryRepository;
+//import com.sparta.jarjarbinks.worldproject.model.services.WorldService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Optional;
+//
+//@RestController
+//public class TestController {
+//
+//    private final WorldService worldService;
+//    private final CountryRepository countryRepository;
+//
+//    @Autowired
+//    public TestController(WorldService worldService, CountryRepository countryRepository) {
+//        this.worldService = worldService;
+//        this.countryRepository = countryRepository;
+//    }
+//
+//    @GetMapping("/test/{countrycode}")
+//    public int TestEndPoint(@PathVariable String countrycode) {
+//        Optional<CountryDTO> country = worldService.getCountryByCode(countrycode);
+//        if (country.isEmpty()) {
+//            // Throw exception
+//            return 0;
+//        } else {
+//            return worldService.getNumberOfPopularLanguageSpeakers(country.get());
+//        }
+//    }
+//
+//
+//
+//
+//}

bdb2aac34715ba460c9fc29f90e974fbbb23520e
Fergus Thompson <FThompson@spartaglobal.com>
2023-11-30 16:15:26 +0000

    Updated patch methods in WorldController

--- src/main/java/com/sparta/jarjarbinks/worldproject/controller/TestController.java
+++ src/main/java/com/sparta/jarjarbinks/worldproject/controller/TestController.java
@@ -1,31 +1,31 @@
-package com.sparta.jarjarbinks.worldproject.controller;
-
-import com.sparta.jarjarbinks.worldproject.model.entities.CountryDTO;
-import com.sparta.jarjarbinks.worldproject.model.repositories.CountryRepository;
-import com.sparta.jarjarbinks.worldproject.model.services.WorldService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class TestController {
-
-    private final WorldService worldService;
-    private final CountryRepository countryRepository;
-
-    @Autowired
-    public TestController(WorldService worldService, CountryRepository countryRepository) {
-        this.worldService = worldService;
-        this.countryRepository = countryRepository;
-    }
-
-    @GetMapping("/test")
-    public void TestEndPoint() {
-        CountryDTO countryDTO = countryRepository.findAll().get(0);
-        System.out.println("Country: " + countryDTO.getName());
-        System.out.println("Number of people: " + worldService.getNumberOfPopularLanguageSpeakers(countryDTO));
-
-    }
-
-}
+//package com.sparta.jarjarbinks.worldproject.controller;
+//
+//import com.sparta.jarjarbinks.worldproject.model.entities.CountryDTO;
+//import com.sparta.jarjarbinks.worldproject.model.repositories.CountryRepository;
+//import com.sparta.jarjarbinks.worldproject.model.services.WorldService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class TestController {
+//
+//    private final WorldService worldService;
+//    private final CountryRepository countryRepository;
+//
+//    @Autowired
+//    public TestController(WorldService worldService, CountryRepository countryRepository) {
+//        this.worldService = worldService;
+//        this.countryRepository = countryRepository;
+//    }
+//
+//    @GetMapping("/test")
+//    public void TestEndPoint() {
+//        CountryDTO countryDTO = countryRepository.findAll().get(0);
+//        System.out.println("Country: " + countryDTO.getName());
+//        System.out.println("Number of people: " + worldService.getNumberOfPopularLanguageSpeakers(countryDTO));
+//
+//    }
+//
+//}
--- src/test/java/com/sparta/jarjarbinks/worldproject/model/services/FergusPutTests.java
+++ src/test/java/com/sparta/jarjarbinks/worldproject/model/services/FergusPutTests.java
@@ -6,30 +6,40 @@ import com.sparta.jarjarbinks.worldproject.model.entities.CityDTO;
 import com.sparta.jarjarbinks.worldproject.model.repositories.CityRepository;
 import com.sparta.jarjarbinks.worldproject.model.repositories.CountryRepository;
 import com.sparta.jarjarbinks.worldproject.model.repositories.CountrylanguageRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class FergusPutTests {
 
-class FergusPutTests {
-
-    private WorldService worldServiceTest;
+    @Mock
     private CityRepository cityRepositoryTest;
+    @Mock
     private CountryRepository countryRepositoryTest;
+    @Mock
     private CountrylanguageRepository countryLanguageRepositoryTest;
 
-    @Test
-    @DisplayName("Check that city is updated")
-    void putCityTest() throws NotFoundException, InvalidArgumentFormatException {
+    private WorldService worldService = new WorldService(cityRepositoryTest, countryRepositoryTest, countryLanguageRepositoryTest);
 
-        CityDTO exisitingCityDTO = new CityDTO();
-        exisitingCityDTO.setId(1);
-        exisitingCityDTO.setDistrict("District To Update");
-        exisitingCityDTO.setName("City To Update");
-        exisitingCityDTO.setPopulation(10000);
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testPutCityWithValidId() throws NotFoundException, InvalidArgumentFormatException {
+        // Mock data
+        CityDTO existingCityDTO = new CityDTO();
+        existingCityDTO.setId(1);
+        existingCityDTO.setDistrict("District To Update");
+        existingCityDTO.setName("City To Update");
+        existingCityDTO.setPopulation(10000);
 
         CityDTO newCityDTO = new CityDTO();
         newCityDTO.setId(2);
@@ -37,23 +47,78 @@ class FergusPutTests {
         newCityDTO.setName("Updated City");
         newCityDTO.setPopulation(20000);
 
-        cityRepositoryTest.save(exisitingCityDTO);
+        cityRepositoryTest.save(existingCityDTO);
 
-        Optional<CityDTO> result = worldServiceTest.putCity(newCityDTO, exisitingCityDTO.getId());
+        // Mock repository behavior
+        when(cityRepositoryTest.findById(existingCityDTO.getId())).thenReturn(Optional.of(existingCityDTO));
 
-        Assertions.assertTrue(result.isPresent());
-        Assertions.assertEquals(newCityDTO, result.get());
+        // Call the method
+        Optional<CityDTO> result = worldService.putCity(newCityDTO, existingCityDTO.getId());
 
+        // Verify repository method calls
+        verify(cityRepositoryTest).findById(existingCityDTO.getId());
+        verify(cityRepositoryTest).save(existingCityDTO);
+
+        // Assertions
+        assertTrue(result.isPresent());
+        assertEquals(newCityDTO, result.get());
     }
-
-    @Test
-    @DisplayName("Check that country is updated")
-    void putCountryTest() {
-    }
-
-    @Test
-    @DisplayName("Check that country language is updated")
-    void putCountryLanguageTest() {
-    }
-
 }
+
+//import com.sparta.jarjarbinks.worldproject.exceptions.InvalidArgumentFormatException;
+//import com.sparta.jarjarbinks.worldproject.exceptions.NotFoundException;
+//import com.sparta.jarjarbinks.worldproject.model.entities.CityDTO;
+//import com.sparta.jarjarbinks.worldproject.model.repositories.CityRepository;
+//import com.sparta.jarjarbinks.worldproject.model.repositories.CountryRepository;
+//import com.sparta.jarjarbinks.worldproject.model.repositories.CountrylanguageRepository;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.Optional;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class FergusPutTests {
+//
+//    private WorldService worldServiceTest;
+//    private CityRepository cityRepositoryTest;
+//    private CountryRepository countryRepositoryTest;
+//    private CountrylanguageRepository countryLanguageRepositoryTest;
+//
+//    @Test
+//    @DisplayName("Check that city is updated")
+//    void putCityTest() throws NotFoundException, InvalidArgumentFormatException {
+//
+//        CityDTO exisitingCityDTO = new CityDTO();
+//        exisitingCityDTO.setId(1);
+//        exisitingCityDTO.setDistrict("District To Update");
+//        exisitingCityDTO.setName("City To Update");
+//        exisitingCityDTO.setPopulation(10000);
+//
+//        CityDTO newCityDTO = new CityDTO();
+//        newCityDTO.setId(2);
+//        newCityDTO.setDistrict("Updated District");
+//        newCityDTO.setName("Updated City");
+//        newCityDTO.setPopulation(20000);
+//
+//        cityRepositoryTest.save(exisitingCityDTO);
+//
+//        Optional<CityDTO> result = worldServiceTest.putCity(newCityDTO, exisitingCityDTO.getId());
+//
+//        Assertions.assertTrue(result.isPresent());
+//        Assertions.assertEquals(newCityDTO, result.get());
+//
+//    }
+//
+//    @Test
+//    @DisplayName("Check that country is updated")
+//    void putCountryTest() {
+//    }
+//
+//    @Test
+//    @DisplayName("Check that country language is updated")
+//    void putCountryLanguageTest() {
+//    }
+//
+//}
